ce46a03984f275fb7319f0896cc5b4f8585f2258
lkranl <thekran95@gmail.com>
2021-03-11 22:29:00 -0500

    little changes

--- examplesJS/js/functions.js
+++ examplesJS/js/functions.js
@@ -1,45 +1,47 @@
-function Saludo(){
+function Saludo() {
     alert("Desde el boton");
 }
 
 var people = [
-    {   
-        id: "12323",
-        firstname : "Malcom", 
-        lastname: "Reynolds",
-        email: "test@email.com"    
-    }
-];
-  
-function testFill(){
-
-    var id = document.getElementById("id").value;
-    var firstname = document.getElementById("firstname").value;
-    var lastname = document.getElementById("lastname").value;
-    var email = document.getElementById("email").value;
-
-    people.push({
-        id: id,
-        firstname : firstname, 
-        lastname: lastname,
-        email: email
-    });
-    console.log(people);
-}
-
-
-function myFunction() {
-    var result = "";
-    people.forEach(function (item) {
-        result += "<li>" + item.id + " " + item.firstname + " " + item.lastname + " " + item.email;
-    });
-
-    document.getElementById("demo").innerHTML = result;
-}
-
-function updateTable(){
-    for(id in people)
     {
-        document.write('<tr><td>' + id + '</td><td>' + people[id].firstname + '</td></tr>');
+        id: "12323",
+        firstname: "Malcom",
+        lastname: "Reynolds",
+        email: "test@email.com",
+        photo: "https://www.gstatic.com/images/branding/product/2x/photos_96dp.png",
+    },
+];
+
+function testFill() {
+    people.push({
+        id: document.getElementById("id").value,
+        firstname: document.getElementById("firstname").value,
+        lastname: document.getElementById("lastname").value,
+        email: document.getElementById("email").value,
+        photo: document.getElementById("photo").value,
+    });
+    myFunction(item);
+    console.log(item);
+}
+
+function myFunction(item) {
+    let tr = document.createElement("tr");
+    let c = 0;
+    item.forEach((i) => {
+        let td = document.createElement("td");
+        console.log(i);
+        td.innerText = i;
+        td.id = c;
+        c += 1;
+        tr.appendChild(td);
+    });
+    document.getElementById("demo").appendChild(tr);
+}
+
+function updateTable() {
+    for (id in people) {
+        document.write(
+            "<tr><td>" + id + "</td><td>" + people[id].firstname + "</td></tr>"
+        );
     }
 }
